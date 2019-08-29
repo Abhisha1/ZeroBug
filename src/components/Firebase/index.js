7b414ff7789dad4cdbbc60aa91b5d7b41db0ab97
Hummad Farooqui <h.farooqui@student.unimelb.edu.au>
2019-08-29 18:53:06 +1000

    Fixed file naming issues and instantiated Firebase global provider

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,18 +1,6 @@
-import app from 'firebase/app';
+import FirebaseContext from './context';
+import Firebase from './firebase';
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
+export default Firebase
 
-class Firebase {
-  constructor() {
-    app.initializeApp(config);
-  }
-}
-
-export default Firebase;
+export { FirebaseContext };
