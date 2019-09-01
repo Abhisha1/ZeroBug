22265413e8bd2bb828683e16b478cb619e5dfa90
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-01 15:40:10 +1000

    adding pages folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
-import Firebase, { FirebaseContext } from './components/Firebase'
+import Firebase, { FirebaseContext } from './pages/Firebase'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
