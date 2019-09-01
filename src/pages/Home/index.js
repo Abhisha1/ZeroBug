80d17a9f73c28cb914204af4500238fa06f2a60c
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-01 16:26:52 +1000

    homepage framework

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import "./home.scss";
+import { Link } from 'react-router-dom';
 
+import * as ROUTES from '../../constants/routes'
 
 class Home extends Component {
 
 	render() {
 		return(
-			<div>
-                HELLO
+			<div className="homepage">
+                <h1>A family artefact register.</h1>
+				<h3>Safe.Secure.Shareable</h3>
+
+					<div class="parallax"></div>
+
+					<div className="text_container">
+					<p>
+						A software solution developed by ZeroBugs to help families share artefacts securely online.
+						
+
+						To get started, <Link to={ROUTES.SIGN_UP}> sign up here</Link>
+					</p>
+					</div>
 			</div>
 		);
 	}
