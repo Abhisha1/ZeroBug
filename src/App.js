9e9c0308544e66d86f7b0630cbc0256238a99a1b
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-01 15:39:50 +1000

    add page folder

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import {
   Route,
  } from 'react-router-dom';
 
-import Navigation from './components/Navigation'
-import LandingPage from './components/Landing';
-import SignUpPage from './components/SignUp';
-import SignInPage from './components/SignIn';
-import PasswordForgetPage from './components/PasswordForget';
-import HomePage from './components/Home';
-import AccountPage from './components/Account';
-import AdminPage from './components/Admin';
+import Navigation from './pages/Navigation'
+import LandingPage from './pages/Landing';
+import SignUpPage from './pages/SignUp';
+import SignInPage from './pages/SignIn';
+import PasswordForgetPage from './pages/PasswordForget';
+import HomePage from './pages/Home';
+import AccountPage from './pages/Account';
+import AdminPage from './pages/Admin';
 
 import * as ROUTES from './constants/routes';
 
