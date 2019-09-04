f39aab1281f3d3c51a386a286483cfd9deaaa955
Hummad Farooqui <h.farooqui@student.unimelb.edu.au>
2019-09-04 16:38:50 +1000

    Chnaged import path for App.js to reflect file structure changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App';
+import App from './App';
 import Firebase, { FirebaseContext } from './pages/Firebase'
 import * as serviceWorker from './serviceWorker';
 
