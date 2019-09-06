75ed33ec0752ce808b6a60eda881a0fa069ec286
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-06 22:00:54 +1000

    styling sign in

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import "./signup.css"
 
-import { withFirebase } from '../../pages/Firebase'
+import { withFirebase } from '../Firebase'
 import * as ROUTES from '../../constants/routes'
+
 
 const INIT_STATE = {
   username: '',
