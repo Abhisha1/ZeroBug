1d36bc8fc092661fd1569b186ba00013d04defb6
Chien-Lin Chen <c.chen89@student.unimelb.edu.au>
2019-09-07 18:29:16 +1000

    get the artifact data and show it on the  web page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -11,43 +11,34 @@ import { withFirebase } from '../Firebase'
 import * as ROUTES from '../../constants/routes'
 import { compose } from 'recompose';
 
-/*
-let testUpload = (ba) => {
-  ba.database().ref('testUploadsJen/' + "testOne").set({
-    name: "lolol",
-    pease: "testing about the uploading job yoo"
-  }, (error) => {
-    if (error) {
-      // The write failed...
-        console.log("Written data FAILED");
-    } else {
-        // Data saved successfully!
-        console.log("Successfully append the data!");
-    }
-  });
+
+class SignUpPage extends Component{
+  state = {
+    artifactName: "No Artifact Name",
+  }
+
+  render() {
+    return (<div>
+      <h1>SignUp</h1>
+      <FirebaseContext.Consumer>
+        {firebase => [
+          <div>
+            <SignUpForm firebase={firebase} />
+            <button onClick={() => firebase.testUploadArtifactData("06","Family Photo6",  "Australia6", "Jen6", "Testing about the uploading job6")}>
+              click to upload the test artifact data</button>
+            <button onClick={() => firebase.testUpdateArtifactData("06", "update name", "update origin", "update owner", "update description")}>
+              click to update the test artifact data</button>
+            <button onClick={() => firebase.testUpdateArtifactData("06", null, null, null, null)}>
+              click to delete the test artifact data</button>
+            <p id  = "artifactInfo">{this.state.artifactName}</p>
+
+            <button onClick={()=>firebase.getArtifactData("05", this)}>Click me to show the artifact Information</button>
+          </div>
+ 
+        ]}
+      </FirebaseContext.Consumer>  
+    </div>)
+  }
 }
-*/
-
-
-const SignUpPage = () => (
-  <div>
-    <h1>SignUp</h1>
-    <FirebaseContext.Consumer>
-      {firebase => [
-        <div>
-          <SignUpForm firebase={firebase} />
-          <button onClick={() => firebase.testUploadArtifactData("06","Family Photo6",  "Australia6", "Jen6", "Testing about the uploading job6")}>
-            click to upload the test artifact data</button>
-          <button onClick={() => firebase.testUpdateArtifactData("06", "update name", "update origin", "update owner", "update description")}>
-            click to update the test artifact data</button>
-          <button onClick={() => firebase.testUpdateArtifactData("06", null, null, null, null)}>
-            click to delete the test artifact data</button>
-        </div>
-
-      ]}
-    </FirebaseContext.Consumer>  
-  
-  </div>
-);
 
 export default SignUpPage;
