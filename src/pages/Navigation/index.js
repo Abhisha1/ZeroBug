531798d83ae0b622fb61a921a4bf4e8af380ccd6
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-12 20:28:42 +1000

    working navbar

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {Navbar, Nav} from 'react-bootstrap';
 import * as ROUTES from '../../constants/routes';
-
-const Naviation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-    </ul>
-  </div>
+const Navigation = () => (
+  <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav>
+      <Nav.Link href={ROUTES.HOME}>Home</Nav.Link>
+      <Nav.Link href={ROUTES.ADMIN}>Admin</Nav.Link>
+      <Nav.Link href={ROUTES.ACCOUNT}>Account</Nav.Link>
+      <Nav.Link href={ROUTES.LANDING}>Landing</Nav.Link>
+      <Nav.Link href={ROUTES.SIGN_IN}>Log In</Nav.Link>
+      <Nav.Link href={ROUTES.SIGN_UP}>Sign Out</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
 );
 
-export default Naviation;
+export default Navigation;
