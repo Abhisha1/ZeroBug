ce603dd063c37c0059c9a05001e69c58a4ff9dfa
Hummad Farooqui <h.farooqui@student.unimelb.edu.au>
2019-09-17 16:33:19 +1000

    Changed import of button from signout page path to signout component

--- src/pages/Navigation/index.js
+++ src/pages/Navigation/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import SignOutButton from '../SignOut';
+import SignOutButton from '../../components/SignOut';
 import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
