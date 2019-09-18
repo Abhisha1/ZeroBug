a39cf0d1e1edec0fa2e3d9cd3532c48e5b56a863
Hummad Farooqui <h.farooqui@student.unimelb.edu.au>
2019-09-18 19:40:32 +1000

    Added basic homepage for testing purpouses

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component{
                 <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
                 <Route path={ROUTES.SIGN_IN} component={SignInPage} />
                 {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} /> */}
-                {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
+                <Route path={ROUTES.HOME} component={HomePage} />
                 {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
                 {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
               </div>
