1c6873edabadc9f1b4786a47f738de716cd4899d
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-21 15:28:47 +1000

    card deck

--- src/components/CardDeck/index.js
+++ src/components/CardDeck/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {CardDeck, Card} from "react-bootstrap";
+import './customdeck.scss';
 class CustomDeck extends Component{
     constructor(props){
         super(props);
@@ -8,9 +9,9 @@ class CustomDeck extends Component{
     render(){
         let cardImg = require('../../assets/bg2.jpg');
         return(
-            <CardDeck>
+            <CardDeck id="cardDeck">
             {this.props.cards.map(item => (
-                <Card key={item.name}>
+                <Card idkey={item.name}>
                 <Card.Img variant="top" src={cardImg} />
                 <Card.Body>
                 <Card.Title>{item.name}</Card.Title>
