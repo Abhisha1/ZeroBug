bce4848ee04dcb32f18cb28d72dff79272c1a8b1
Chien-Lin Chen <c.chen89@student.unimelb.edu.au>
2019-09-23 01:36:19 +1000

    added some styles, the your profile section is almost done

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
-//import "./account.scss";
+import "./account.scss";
 import {FirebaseContext} from "../../components/Firebase";
 
 class AccountPage extends Component {
 
     render() {
 		return(
-			<div>
-                <h1>aaaaabbbbbb</h1>
-        
+			<div id="profileContainer">
+				<div id="topWraper">
+					<h1 id="account-heading">Your Profile</h1>
+					<div id="topContainer">
+						<div id="avatarContainer">
+							<img id="avatarBox"></img>
+							<button className="aButton">Change Avatar</button>
+						</div>
+						<div id="profileInformationContainer">
+							<h2 id="profile-name">Name</h2>
+							<button className="aButton" id="changePasswordButton">Change Password</button>
+						</div>
+					</div>
+				</div>
+
+				<div>
+					<h1 id="account-heading">Artifacts Owned</h1>
+				</div>
+
+				<div>
+					<h1 id="account-heading">Manage Families</h1>
+				</div>
 
 			</div>
+			
 		);
 	}
 }
