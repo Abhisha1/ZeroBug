07e01861d25ee8fe03d7ab97268019cb54167bd8
Chien-Lin Chen <c.chen89@student.unimelb.edu.au>
2019-09-22 21:47:44 +1000

    set up for account page

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import SignUpPage from './pages/SignUp';
 import SignInPage from './pages/SignIn';
 import CreateFamilyPage from './pages/CreateFamily';
 import Artifact from './pages/Artifact';
+import AccountPage from './pages/Account';
 import PasswordForgetPage from './pages/PasswordForget';
 import HomePage from './pages/Home';
-import AccountPage from './pages/Account';
 import AdminPage from './pages/Admin';
 
 import * as ROUTES from './constants/routes';
@@ -29,9 +29,9 @@ const App = () => (
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.CREATE_FAMILY} component={CreateFamilyPage} />
       <Route path={ROUTES.ARTIFACT} component={Artifact} />
+      {<Route path={ROUTES.ACCOUNT} component={AccountPage} />}
       {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} /> */}
       {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
-      {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
       {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
     </div>
   </Router>
---
+++ src/pages/Account/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+//import "./account.scss";
+import {FirebaseContext} from "../../components/Firebase";
+
+class AccountPage extends Component {
+
+    render() {
+		return(
+			<div>
+                <h1>aaaaabbbbbb</h1>
+        
+
+			</div>
+		);
+	}
+}
+
+export default AccountPage;
