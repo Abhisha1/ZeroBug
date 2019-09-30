05d4e89217dbeafe5226deb87d72f14c833bd3c6
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2019-09-30 22:09:25 +1000

    semi-colon bug fix

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -26,7 +26,7 @@ const withAuthentication = Component => {
     render() {
       return (
           <AuthUserContext.Provider value={this.state.authUser}>
-              <Component {...this.props} />;
+              <Component {...this.props} />
           </AuthUserContext.Provider>
       );
   }
