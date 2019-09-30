c452e48f695394454890d94590d9e971c51c32e8
Hummad Farooqui <h.farooqui@student.unimelb.edu.au>
2019-09-30 17:56:33 +1000

    Responsive CSS using Material UI, incomplete

---
+++ src/components/SignUpForm/SignUpForm2.js
@@ -0,0 +1,198 @@
+import React from 'react';
+import clsx from 'clsx';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import CreateRoundedIcon from '@material-ui/icons/CreateRounded';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import yellow from '@material-ui/core/colors/yellow';
+import { withRouter } from 'react-router-dom';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const INIT_STATE = {
+  username: '',
+  email: ' ',
+  password: '',
+  confirmpassword: '',
+  error: null,
+};
+
+const primary = yellow[800];
+
+const useStyles = makeStyles(theme => ({
+  overrides: {
+    MuiTypography: {
+      colorPrimary: primary,
+    },
+  },
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: primary,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: primary,
+  },
+}));
+
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    userName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    showPassword: false,
+  });
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  const onSubmit = event => {
+    const { username, email, password } = this.state;
+
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then(authUser => {
+        this.setState({ ...INIT_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      })
+
+      event.preventDefault();
+  };
+
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <CreateRoundedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h4" color='primary' align='center'>
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="userName"
+                variant="outlined"
+                required
+                fullWidth
+                id="userName"
+                label="User Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl variant='outline' required='true'>
+                <InputLabel>Email</InputLabel>
+                <OutlinedInput
+                  id="email"
+                  type='email'
+                  value={values.email}
+                  onChange={handleChange('email')}
+                />
+            </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl variant='outlined' required='true'>
+                <InputLabel htmlFor="adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="adornment-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl margin='normal' variant='outlined' required='true'>
+                <InputLabel htmlFor="adornment-password">Confirm Password</InputLabel>
+                <OutlinedInput
+                  id="adornment-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.confirmPassword}
+                  onChange={handleChange('confirmPassword')}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="center">
+            <Grid item>
+              <Link href="/signin" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
