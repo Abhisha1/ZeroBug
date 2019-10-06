cdf9f11174b002fc20cbe6281a77f570513d8447
umawani <umawani@DESKTOP-1NQPFIL.localdomain>
2019-10-06 20:14:40 +1100

    Working flexbox implementation of cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import Cards from "./../../components/Card";
-
-
+import "./container.scss"
 
 class HomePage extends Component {
     render(){
         return(
-          <div>
-            <Cards/>
+          <div class="container">
+                <Cards/>
           </div>
         );
+
     }
 }
 
