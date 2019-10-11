1d1253aa4af56530325f9b394744d5c546a62048
umawani <umawani@DESKTOP-1NQPFIL.localdomain>
2019-10-11 15:09:49 +1100

    work in progress: paper

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 import Cards from "./../../components/Card";
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import "./container.scss"
 
+
+
+
 class HomePage extends Component {
+
+     var styles = makeStyles(theme => ({
+        paper: {
+        padding: 30,
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'row',
+    },
+    }));
+
     render(){
         return(
-          <div class="container">
+            <Paper className={classes.paper}>
                 <Cards/>
                 <Cards/>
                 <Cards/>
                 <Cards/>
                 <Cards/>
-          </div>
+          </Paper>
         );
 
     }
