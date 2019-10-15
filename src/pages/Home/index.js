cf4a6d02ae91eab68c9fdf92fc47d568d55091a7
umawani <umawani@DESKTOP-1NQPFIL.localdomain>
2019-10-15 21:07:46 +1100

    Initial slider added, a LOT of bugs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
-import Cards from "./../../components/Card";
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
-import clsx from 'clsx';
+import RenderFamilies from "./renderFamilies";
+import Cards from "./../../components/Card";
 import "./container.scss"
+import "./account.scss"
 
-const styles = makeStyles(theme => ({
-   paper: {
-   padding: 30,
-   display: 'flex',
-   overflow: 'auto',
-   flexDirection: 'row',
-},
-}));
 
 
 class HomePage extends Component {
@@ -23,6 +16,11 @@ class HomePage extends Component {
     render(){
         return(
             <div>
+            <Paper id="familyPaper">
+                <div>
+                    <RenderFamilies />
+                </div>
+            </Paper>
                 <Divider />
                 <Typography variant="h2" align="center">
                     Artifacts
---
+++ src/pages/Home/renderFamilies.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { withFirebase } from '../../components/Firebase';
+import { withRouter } from 'react-router-dom';
+import FamilySlider from "./cardSlider";
+
+class RenderFamilies extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            familyList: null,
+            username: null,
+            familyImageURL: [],
+            dataReady: false
+        }
+    }
+
+    // get the families that the user managed
+    componentDidMount =() => {
+        this.props.firebase.auth.onAuthStateChanged((user)=>{
+            if(user){
+                this.setState({username: user.displayName});
+                this.props.firebase.getYourManagedFamilyName(this, this.state.username);
+            }
+        })
+    }
+
+    render(){
+
+        let cardData = []
+
+        if(this.state.dataReady){
+            for(let i = 0; i < this.state.familyList.length; i++){
+                cardData.push(
+                    {
+                        avatar: this.state.familyImageURL[i],
+                        name: this.state.familyList[i]
+                    }
+                )
+            }
+        }
+
+        return(
+            <div>
+                <div id="familiesWrapper">
+					<h1 id="account-heading">Your Managed Families</h1>
+                    <FamilySlider cards={
+                            cardData
+					}/>
+				</div>
+            </div>
+        )
+    }
+}
+const renderThings = withRouter(withFirebase(RenderFamilies));
+export default renderThings;
