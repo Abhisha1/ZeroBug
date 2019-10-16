d9e62a41120d8078388ddad39c3eca9ffd9b4b27
Chien-Lin Chen <c.chen89@student.unimelb.edu.au>
2019-10-16 15:38:14 +1100

    add the slider in to home page

---
+++ src/pages/Home/renderFamilies.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { withFirebase } from '../../components/Firebase';
+import { withRouter } from 'react-router-dom';
+import FamilySlider from "./cardSlider";
+
+class RenderFamilies extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            familyList: null,
+            username: null,
+            familyImageURL: [],
+            dataReady: false,
+            cardData: null,
+        }
+    }
+
+    // get the families that the user managed
+    componentDidMount =() => { 
+        this.props.firebase.auth.onAuthStateChanged((user)=>{
+            if(user){
+                this.setState({username: user.displayName});
+                this.props.firebase.getYourFamilyNames(this, this.state.username);
+            }
+        })  
+    }
+
+    render(){
+
+        return(
+            <div>
+                <div id="familiesWrapper">
+					<h1 id="account-heading">Your Families</h1>
+                    <FamilySlider cards=
+                        {this.state.dataReady?
+                            this.state.cardData:[]
+                        }
+					/>
+				</div>
+            </div>
+        )
+    }
+}
+const renderThings = withRouter(withFirebase(RenderFamilies));
+export default renderThings;
