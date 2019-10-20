782e6f61599f0953f8dcd5965c9a900cbd930ada
Hummad Farooqui <h.farooqui@student.unimelb.edu.au>
2019-10-20 17:29:22 +1100

    Cleaned up landing page, added alternate for landing photo

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { SignUpLink } from '../../components/SignUpForm/SignUpForm';
 import SignInForm from "../../components/SignInForm/SignInForm";
 import Grid from '@material-ui/core/Grid';
-import "./signin.scss";
 
 /**
  * The page that handles existing users signing in/logging in to their account
